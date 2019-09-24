849824f7dfb9501b49225f34ce248592d7e8a9e3
MasatakaHisano-git <55529202+MasatakaHisano-git@users.noreply.github.com>
2019-09-24 16:34:01 +0900

    modified download test script

--- express-api/app/routes/v1/uri_2.js
+++ express-api/app/routes/v1/uri_2.js
@@ -1,10 +1,26 @@
 var express = require('express');
 var router = express.Router();
+//const fs = require('fs');
 
-router.get('/test', function (req, res) {
+router.get('/test', function(req, res) {
+
+  var filePath = "/root/express-api/app/routes/v1"
+  var fileName = "test_10mb.txt";
+  var file = "/root/express-api/app/routes/v1/test.txt"
+  //try {
+  //  fs.statSync(file);
+  //  console.log("file exist");
+  //  return true
+  //} catch(err) {
+  //  if(err.code === 'ENOENT') return false
+  //  console.log("file doesnot exist");
+  //}
+  res.attachment(file);
+  res.download(file);
+  
   res.json({
-    "id": 1,
-    "name" : "QBSa"
+    "id" : 4,
+    "name": "QBS4"
   });
 });
 
